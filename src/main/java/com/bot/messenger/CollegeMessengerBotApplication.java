ea2aada12db568bd0348e9d657298fa391336435
rajamichael80 <udhayan.addon@gmail.com>
2019-07-29 04:02:02 +0530

    base project code added

---
+++ src/main/java/com/bot/messenger/CollegeMessengerBotApplication.java
@@ -0,0 +1,13 @@
+package com.bot.messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollegeMessengerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollegeMessengerBotApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bot/messenger/model/fb/SendMessageAcknowledgement.java
@@ -0,0 +1,32 @@
+
+package com.bot.messenger.model.fb;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class SendMessageAcknowledgement {
+
+@SerializedName("recipient_id")
+@Expose
+private String recipientId;
+@SerializedName("message_id")
+@Expose
+private String messageId;
+
+public String getRecipientId() {
+return recipientId;
+}
+
+public void setRecipientId(String recipientId) {
+this.recipientId = recipientId;
+}
+
+public String getMessageId() {
+return messageId;
+}
+
+public void setMessageId(String messageId) {
+this.messageId = messageId;
+}
+
+}
