92f99d52d163783c47f45a7d94f3762f47f89a15
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-06 07:41:11 +0530

    adding database connection

--- src/main/java/com/bot/messenger/controller/ChatBotHookController.java
+++ src/main/java/com/bot/messenger/controller/ChatBotHookController.java
@@ -1,16 +1,30 @@
 package com.bot.messenger.controller;
 
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bot.messenger.dao.DataBaseConnection;
 
 @RestController
 public class ChatBotHookController {
 
 	@GetMapping("/")
 	public ResponseEntity<?> sayConnected() {
-		return new ResponseEntity<String>("Default controller is Listening", HttpStatus.OK);
+		String schemaName = null;
+		DataBaseConnection db= new DataBaseConnection();
+		try {
+			Connection connection = db.getConnection();
+			schemaName = connection.getSchema();
+		} catch (URISyntaxException | SQLException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>("Default controller is Listening schemaName :"+schemaName, HttpStatus.OK);
 	}
 }
 //https://messengerdevelopers.com/resources/messaging
