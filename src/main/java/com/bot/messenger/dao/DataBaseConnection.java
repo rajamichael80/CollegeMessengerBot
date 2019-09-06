2731651315d543340b7fe577069bf97000a259ff
rajamichael80 <53397156+rajamichael80@users.noreply.github.com>
2019-09-06 15:04:08 +0530

    Update DataBaseConnection.java
    adding logs

--- src/main/java/com/bot/messenger/dao/DataBaseConnection.java
+++ src/main/java/com/bot/messenger/dao/DataBaseConnection.java
@@ -6,11 +6,13 @@ import java.sql.*;
 public class DataBaseConnection {
 	public static Connection getConnection() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-
+System.out.println("dbUri = "+dbUri);
 		
-		  String username = dbUri.getUserInfo().split(":")[0]; String password =
-		  dbUri.getUserInfo().split(":")[1]; String dbUrl = "jdbc:postgresql://" +
-		  dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +"?sslmode=require";
+		  String username = dbUri.getUserInfo().split(":")[0]; 
+		System.out.println("username="+username);
+
+		  String password =  dbUri.getUserInfo().split(":")[1]; 
+		  String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +"?sslmode=require";
 		  return DriverManager.getConnection(dbUrl, username, password);
 		
 	}
