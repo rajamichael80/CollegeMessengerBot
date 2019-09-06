a7ac79eeacb8aa85ac5736221db76a8410888321
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-06 08:56:11 +0530

    ddd

--- src/main/java/com/bot/messenger/dao/DataBaseConnection.java
+++ src/main/java/com/bot/messenger/dao/DataBaseConnection.java
@@ -1,13 +1,12 @@
 package com.bot.messenger.dao;
 
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
 	public static Connection getConnection() throws URISyntaxException, SQLException {
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		//URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
 		/*
 		 * String username = dbUri.getUserInfo().split(":")[0]; String password =
