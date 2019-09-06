01ab0bfbbeb861224ddacbe0d0779f0de783b0db
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-06 08:25:31 +0530

    adding DATABASE_URL instead of JDBC URL

--- src/main/java/com/bot/messenger/dao/DataBaseConnection.java
+++ src/main/java/com/bot/messenger/dao/DataBaseConnection.java
@@ -1,15 +1,19 @@
 package com.bot.messenger.dao;
 
+import java.net.URI;
 import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DataBaseConnection {
-	public  Connection getConnection() throws URISyntaxException, SQLException {
-	    String dbUrl = System.getenv("JDBC_DATABASE_URL");
-	    return DriverManager.getConnection(dbUrl);
-	    
-	    
+	public Connection getConnection() throws URISyntaxException, SQLException {
+		URI dbUri = new URI(System.getenv("DATABASE_URL"));
+
+	    String username = dbUri.getUserInfo().split(":")[0];
+	    String password = dbUri.getUserInfo().split(":")[1];
+	    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
+
+	    return DriverManager.getConnection(dbUrl, username, password);
 	}
 }
