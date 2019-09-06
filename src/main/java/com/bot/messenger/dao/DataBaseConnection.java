d018d21678694c52add1e63eaad88e7bb8de31d4
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-06 08:43:03 +0530

    aaa

--- src/main/java/com/bot/messenger/dao/DataBaseConnection.java
+++ src/main/java/com/bot/messenger/dao/DataBaseConnection.java
@@ -8,10 +8,10 @@ public class DataBaseConnection {
 	public String getConnection() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
 
-	    String username = dbUri.getUserInfo().split(":")[0];
-	    String password = dbUri.getUserInfo().split(":")[1];
+	    //String username = dbUri.getUserInfo().split(":")[0];
+	    //String password = dbUri.getUserInfo().split(":")[1];
 	    //String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() + "?sslmode=require";
 
-	    return username;
+	    return dbUri.getPath();
 	}
 }
