03f5badfdab2e9689c0202e96caf97c9fc9f3a17
rajamichael80 <53397156+rajamichael80@users.noreply.github.com>
2019-09-09 15:46:49 +0530

    Update DataBaseConnection.java
    ghj

--- src/main/java/com/bot/messenger/dao/DataBaseConnection.java
+++ src/main/java/com/bot/messenger/dao/DataBaseConnection.java
@@ -5,15 +5,16 @@ import java.net.URISyntaxException;
 import java.sql.*;
 public class DataBaseConnection {
 	public static Connection getConnection() throws URISyntaxException, SQLException {
-		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-System.out.println("dbUri = "+dbUri);
+		/*URI dbUri = new URI(System.getenv("DATABASE_URL"));
+		System.out.println("dbUri = "+dbUri);
 		
 		  String username = dbUri.getUserInfo().split(":")[0]; 
 		System.out.println("username="+username);
 
 		  String password =  dbUri.getUserInfo().split(":")[1]; 
 		  String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath() +"?sslmode=require";
-		  return DriverManager.getConnection(dbUrl, username, password);
+		  return DriverManager.getConnection(dbUrl, username, password);*/
+		return null;
 		
 	}
 }
