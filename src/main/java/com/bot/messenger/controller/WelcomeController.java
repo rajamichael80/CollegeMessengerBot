0bf8136010988292509ecd1b9883dfdbc75652a0
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-13 01:03:54 +0530

    fdf

--- src/main/java/com/bot/messenger/controller/WelcomeController.java
+++ src/main/java/com/bot/messenger/controller/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 
 	// inject via application.properties
