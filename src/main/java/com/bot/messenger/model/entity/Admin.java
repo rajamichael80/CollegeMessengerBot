e4d71c37e3f0975fea0bf42ab7473cba6324e894
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-22 23:04:35 +0530

    nvghg

--- src/main/java/com/bot/messenger/model/entity/Admin.java
+++ src/main/java/com/bot/messenger/model/entity/Admin.java
@@ -7,7 +7,7 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "contact")
+@Table(name = "admin")
 public class Admin {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
