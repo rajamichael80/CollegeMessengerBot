e176b43962b3443b87b0a0c1f89a730caf3d4c98
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-23 10:57:37 +0530

    ggfc

--- src/main/java/com/bot/messenger/service/UserService.java
+++ src/main/java/com/bot/messenger/service/UserService.java
@@ -26,6 +26,7 @@ public class UserService implements IUserService{
 
 		logger.debug("---inside save user -----");
 		User user = new User();
+		user.setUserId(Integer.valueOf(userDetail.getId()));
 		user.setFirstName(userDetail.getFirstName());
 		user.setLastName(userDetail.getLastName());
 		User u = userRepository.save(user);
