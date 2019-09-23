9fb8555cc0e239267ad99d9582a5c1f17c7aaada
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-23 10:48:17 +0530

    dsff

--- src/main/java/com/bot/messenger/model/entity/User.java
+++ src/main/java/com/bot/messenger/model/entity/User.java
@@ -2,12 +2,13 @@ package com.bot.messenger.model.entity;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class User {
 	@Id
-    @GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int userId;
 	private String firstName;
 	private String lastName;
