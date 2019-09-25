94910bad9549300381766bab3d5465b655ec57c1
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-09-25 10:13:14 +0530

    cxcc

--- src/main/java/com/bot/messenger/model/entity/User.java
+++ src/main/java/com/bot/messenger/model/entity/User.java
@@ -7,7 +7,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 @Entity
-@Table(name = "user", schema = "newprinceapp")
+@Table(name = "user")
 public class User {
 	@Id
     @GeneratedValue(strategy = GenerationType.AUTO)
