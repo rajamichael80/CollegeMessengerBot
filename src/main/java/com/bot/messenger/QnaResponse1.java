b7f013d340f4f10a99dd62b26c0643ab1f3dfdbf
rajamichael80 <53397156+rajmichael80@users.noreply.github.com>
2019-10-08 20:07:19 +0530

    cfbfbb

--- src/main/java/com/bot/messenger/QnaResponse1.java
+++ src/main/java/com/bot/messenger/QnaResponse1.java
@@ -13,40 +13,42 @@ public class QnaResponse1 {
 
 	public String getJsonResponse(String senderId, String requestText, UserDetail userDetail) {
 		String jsonResponse = "";
-		String key = getActualKeyword(requestText);
-		/*
-		 * switch (getActualKeyword(requestText)) { case "hi": jsonResponse =
-		 * String.format(
-		 * "{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }"
-		 * , "Hi " + userDetail.getFirstName() + " " + userDetail.getLastName()
-		 * +", Welcome to New Prince Shri Bhavani College of ENgineering and Technology, How can I help you?"
-		 * ); break; case "course": jsonResponse =
-		 * "{ \"recipient\":{ \"id\":\"recipientId\" }, \"message\":{ \"attachment\":{ \"type\":\"template\", \"payload\":{ \"template_type\":\"generic\", \"elements\":[ { \"title\":\"Courses\", \"image_url\":\"https://college-messanger-bot.herokuapp.com/img/courses_offered.gif\", \"subtitle\":\"Here is cousre details\", \"buttons\":[ { \"type\":\"postback\",  \"title\":\"MCA\" ,\"payload\":\"MCADemo\"}, { \"type\":\"web_url\", \"url\":\"http://www.newprinceshribhavani.com/computer-science.php\", \"title\":\"Information Technology\" }, { \"type\":\"postback\", \"title\":\"Any Other Query\", \"payload\":\"call our representative\" } ] }  ] } } } }"
-		 * ; break; case "time": jsonResponse = String.
-		 * format("{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }"
-		 * ,"College start at 9.00 AM and ends at 4.30PM"); break;
-		 * 
-		 * case "call our representative": jsonResponse = String.format(
-		 * "{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }"
-		 * , "Hi " + userDetail.getFirstName() + " " + userDetail.getLastName()
-		 * +", puoi contattare l'assistenza clienti al numero 800.142.142 (per le chiamate da rete fissa nazionale) oppure al numero 0039.015.24.34.617 (per le chiamate dall'estero e da telefono cellulare) attivi dal luneda al venerda dalle ore 8.00 alle 21.00. Hai bisogno di altre informazioni?"
-		 * ); break; case "bye": case "": jsonResponse = String.format(
-		 * "{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }"
-		 * , "Grazie " + userDetail.getFirstName() + " " + userDetail.getLastName()
-		 * +", Speriamo di aver risposto alle tue domamde.  Buona giornata "); break;
-		 * 
-		 * default:
-		 * 
-		 * jsonResponse =
-		 * "{ \"recipient\":{ \"id\":\"recipientId\" }, \"message\":{ \"attachment\":{ \"type\":\"template\", \"payload\":{ \"template_type\":\"button\", \"text\":\"I could not understand the request, if you wish I can put you in touch with one of our assistants.\", \"buttons\":[ { \"type\":\"phone_number\", \"title\":\"Contact Number\", \"payload\":\"+390152434600\" } ] } } } }"
-		 * ; break; }
-		 */
-		JSONConversionUtil conversionUtil = new JSONConversionUtil();
+		switch (getActualKeyword(requestText)) {
+		case "hi":
+			jsonResponse = String.format(
+					"{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }",
+					"Hi " + userDetail.getFirstName() + " " + userDetail.getLastName()
+							+ ", Welcome to New Prince Shri Bhavani College of ENgineering and Technology, How can I help you?");
+			break;
+		case "course":
+			jsonResponse = "{ \"recipient\":{ \"id\":\"recipientId\" }, \"message\":{ \"attachment\":{ \"type\":\"template\", \"payload\":{ \"template_type\":\"generic\", \"elements\":[ { \"title\":\"Courses\", \"image_url\":\"https://college-messanger-bot.herokuapp.com/img/courses_offered.gif\", \"subtitle\":\"Here is cousre details\", \"buttons\":[ { \"type\":\"postback\",  \"title\":\"MCA\" ,\"payload\":\"MCADemo\"}, { \"type\":\"web_url\", \"url\":\"http://www.newprinceshribhavani.com/computer-science.php\", \"title\":\"Information Technology\" }, { \"type\":\"postback\", \"title\":\"Any Other Query\", \"payload\":\"call our representative\" } ] }  ] } } } }";
+			break;
+		case "time":
+			jsonResponse = String.format(
+					"{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }",
+					"College start at 9.00 AM and ends at 4.30PM");
+			break;
 
-		Map<String, String> qnaResponse = conversionUtil.convertXmlToObject().getQnaResponse();
-		System.out.println("----qnaResponse----" + qnaResponse);
-		jsonResponse = qnaResponse.get(key);
-		System.out.println("----key----" + key);
+		case "call our representative":
+			jsonResponse = String.format(
+					"{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }",
+					"Hi " + userDetail.getFirstName() + " " + userDetail.getLastName()
+							+ ", puoi contattare l'assistenza clienti al numero 800.142.142 (per le chiamate da rete fissa nazionale) oppure al numero 0039.015.24.34.617 (per le chiamate dall'estero e da telefono cellulare) attivi dal luneda al venerda dalle ore 8.00 alle 21.00. Hai bisogno di altre informazioni?");
+			break;
+		case "bye":
+		case "":
+			jsonResponse = String.format(
+					"{ \"recipient\": { \"id\": \"recipientId\" }, \"message\": { \"text\": \"%s\" } }",
+					"Grazie " + userDetail.getFirstName() + " " + userDetail.getLastName()
+							+ ", Speriamo di aver risposto alle tue domamde.  Buona giornata ");
+			break;
+
+		default:
+
+			jsonResponse = "{ \"recipient\":{ \"id\":\"recipientId\" }, \"message\":{ \"attachment\":{ \"type\":\"template\", \"payload\":{ \"template_type\":\"button\", \"text\":\"I could not understand the request, if you wish I can put you in touch with one of our assistants.\", \"buttons\":[ { \"type\":\"phone_number\", \"title\":\"Contact Number\", \"payload\":\"+390152434600\" } ] } } } }";
+			break;
+		}
+
 		jsonResponse = jsonResponse.replace("recipientId", senderId);
 		logger.info("jsonresponse>>>>{}", jsonResponse);
 		byte[] utf8Bytes;
